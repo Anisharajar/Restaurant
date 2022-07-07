7265eba624a3b9014b4167a30dd1e35bd3fc0607
Anisharajar <anisharajar.2002@gmail.com>
2022-07-07 22:49:34 +0530

    View food

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewfoodComponent } from './viewfood/viewfood.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewfoodComponent
   ],
   imports: [
     BrowserModule,
