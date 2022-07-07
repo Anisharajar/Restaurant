db7b92b66a7b9240fc65d47eaf1f312dedf8e5af
Anisharajar <anisharajar.2002@gmail.com>
2022-07-07 23:06:48 +0530

    search food

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewfoodComponent } from './viewfood/viewfood.component';
+import { SearchfoodComponent } from './searchfood/searchfood.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewfoodComponent
+    ViewfoodComponent,
+    SearchfoodComponent
   ],
   imports: [
     BrowserModule,
